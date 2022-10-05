912b269e328366cd9adccb68e3dba2b818116973
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-04 23:31:29 -0300

    Add files via upload

---
+++ PraticandoFOR_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PraticandoFOR_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    
+            
+         /*para num de 0 ate 10 passo 1 faca*/
+            for(int n=0;n<10;n++)
+            {
+                Console.WriteLine("{0} Rian Lima",n);               
+            }
+            
+        }
+    }
+}
