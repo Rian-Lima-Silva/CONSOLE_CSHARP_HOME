3fb954942f0707ca25d4b59a10fc94fc544ea1a7
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-04 21:42:14 -0300

    Add files via upload

---
+++ PraticandoSE_01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PraticandoSE_01 // IF Uma Variavel   
+{
+    class Praticando01 
+    {  
+        static void Main(string[] args)
+        {
+            int nota,n1;
+            string resultado="Reprovado";
+
+            Console.WriteLine("Digite o Primeiro Número:");
+            n1 = int.Parse(Console.ReadLine());
+        
+            nota = n1;
+
+            if(nota>=60)
+            {
+               resultado = "Aprovado";
+            }
+
+            Console.WriteLine("Resultado: {0}",resultado);
+        }
+
+        static void L()//LimpaTela
+        {
+            Console.Clear();
+        }
+ 
+    }
+}
