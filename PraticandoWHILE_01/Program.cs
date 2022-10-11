9968a8ddced7e246f6ee2da25aa3d79ada602a03
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-10 23:42:17 -0300

    Add files via upload

---
+++ PraticandoWHILE_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PraticandoWHILE_01
+{
+    /*FOR: usado quando souber o número de repitições | WHILE: Quando não souber o número de repetições*/
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num=new int[10];
+            int i=9;
+            
+
+           while(i>0)
+            {
+                Console.WriteLine("Número de Repetições: {0}",i);
+                num[i]=0;
+                Console.WriteLine("Teste | {0}",num[i]);
+                
+                i--;
+            }               
+                        
+        }
+    }
+}
