c0b174da4825409fc136d67554396e982e45254f
RianLima2009 <113356176+RianLima2009@users.noreply.github.com>
2022-10-10 23:44:11 -0300

    Add files via upload

---
+++ PraticandoGT_01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PraticandoGT_01
+{
+    class Study
+    {
+        static void Main(string[] args)
+        {
+            int tempo=0;
+            char escolha=' ';
+            string op=" ";
+
+            inicio:
+
+            Console.Clear();
+
+            Console.WriteLine("Belo Horizonte/MG a Vitória/ES");
+            Console.WriteLine("Escola o transporte:\n [a] Avião \n [c] Carro \n [o] Ônibus");
+            escolha = char.Parse(Console.ReadLine());
+
+            switch(escolha){
+                case 'a':
+                case 'A':
+                    op="Avião";
+                    tempo=50;
+                    break;
+                case 'c':
+                case 'C':
+                    op="Carro";
+                    tempo=480;
+                    break;
+                case'o':
+                case 'O':
+                    tempo=660;
+                    op="Ônibus";
+                    break;
+                default:
+                    tempo=-1;
+                    break;
+            }
+
+            if(tempo<0){
+                Console.WriteLine("Transporte Indisponivel");
+            }
+            else{
+                Console.WriteLine("Transporte: {0} Tempo Estimado: {1} minutos\n",op,tempo);
+            }
+
+            Console.WriteLine("Você deseja fazer um novo calculo: [s/n]");
+            escolha =char.Parse(Console.ReadLine());
+
+            if(escolha=='s'|| escolha=='S'){
+                goto inicio;
+            }else{
+                Console.Clear();
+                Console.WriteLine("Fim do Programa!");
+            }
+        }
+    }
+}
